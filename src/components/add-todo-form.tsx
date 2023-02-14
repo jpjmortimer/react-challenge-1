138a6166473708dd96bcada5a7ae68c5860a5673
jpjmortimer <jpjmortimer@gmail.com>
2023-02-14 23:07:26 +0000

    feat: Main functionality

--- src/components/add-todo-form.tsx
+++ src/components/add-todo-form.tsx
@@ -1,10 +1,47 @@
+import { useState, FormEvent } from "react";
 import type { Todo } from "../app.types";
-import { convertStringToTags } from "../utils";
+import { convertStringToTags, createId } from "../utils";
 
 type AddTodoFormProps = {
   onAdd: (todo: Todo) => void;
 };
 
 export function AddTodoForm({ onAdd }: AddTodoFormProps) {
-  return <form>{/* implement me */}</form>;
+  const [title, setTitle] = useState("");
+  const [tags, setTags] = useState("");
+
+  function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+    const todo: Todo = {
+      id: createId(),
+      completed: false,
+      title: title,
+      tags: convertStringToTags(tags)
+    };
+
+    setTitle("");
+    setTags("");
+
+    onAdd(todo);
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        name="title"
+        placeholder="Something to do..."
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        required
+      />
+      <input
+        type="text"
+        name="tags"
+        placeholder="Tags (comma separated)"
+        value={tags}
+        onChange={(e) => setTags(e.target.value)}
+      />
+      <button type="submit">Add</button>
+    </form>
+  );
 }
--- src/utils.ts
+++ src/utils.ts
@@ -2,7 +2,14 @@
  * This function converts a comma separated string to a list of unique, trimmed strings.
  */
 export function convertStringToTags(entry: string): string[] {
-  // implement me
+  // this could be more concise but I've left it this way for readability
+  const trimmedTags = entry.split(",").map((tag) => tag.trim());
+
+  const cleanedTags = trimmedTags.filter((tag) => tag != "");
+
+  const uniqueTags = [...new Set(cleanedTags)];
+
+  return uniqueTags;
 }
 
 export function createId(): number {
